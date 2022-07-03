1b5fda4f70c834b41f03b55f4b60225c86ebfccc
armin <arminkheirkhahan@gmail.com>
2022-07-03 15:28:59 +0430

    add bootstrap

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,12 @@
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="/products/1">test</a> to read the documentation</p>
+<div class="card" style="width: 18rem;">
+	<img src="..." class="card-img-top" alt="..." />
+	<div class="card-body">
+		<h5 class="card-title">Card title</h5>
+		<p class="card-text">
+			Some quick example text to build on the card title and make up the bulk of the card's content.
+		</p>
+		<a href="/products/1" class="btn btn-primary">Go somewhere</a>
+	</div>
+</div>
--- svelte.config.js
+++ svelte.config.js
@@ -1,14 +1,33 @@
+import preprocess from 'svelte-preprocess';
 import adapter from '@sveltejs/adapter-auto';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	},
 	// preprocess: preprocess({
 	// 	preserve: ['ld+json']
 	// 	// ...
 	// })
+	kit: {
+		adapter: adapter(),
+
+		vite: {
+			css: {
+				preprocessorOptions: {
+					scss: {
+						additionalData: '@use "src/variables.scss" as *;'
+					}
+				}
+			}
+		}
+	},
+
+	preprocess: [
+		preprocess({
+			scss: {
+				prependData: '@use "src/variables.scss" as *;'
+			}
+		})
+	]
 };
 
 export default config;
